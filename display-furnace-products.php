5d70b366e3a0edcc5759f9b60aa7cf3f09dd0d41
Blackwood <zlovlokvi@gmail.com>
2023-01-11 19:02:03 -0800

    Git Integration - not working. Stabilized plugin after changes. Can now sort from price ASC.

--- display-furnace-products.php
+++ display-furnace-products.php
@@ -38,47 +38,61 @@ function display_furnace_products($atts) {
     ],
   ];
 // Run the query
-  $product_query = new WP_Query($query_args);
-// Build the output
-  $output = '<div class="product_column">';
+$product_query = new WP_Query($query_args);
 
-  if ($product_query->have_posts()) {
+// Declare the $products array and output variable
+$output = '<div class="product_column">';
+$products = array();
+
+if ($product_query->have_posts()) {
   while ($product_query->have_posts()) {
     $product_query->the_post();
     $title = get_the_title();
     $excerpt = get_the_excerpt();
-  	//$price = 0;
-  if ($tier == '1'){
-  $price = get_field('hvac_product_price_1');}
-	  elseif ($tier == '2'){
-		  $price = get_field('hvac_product_price_2');}
-	  		elseif ($tier == '3'){
-		  		$price = get_field('hvac_product_price_3');}
-	  				elseif ($tier == '4'){
-		  				$price = get_field('hvac_product_price_4');}
-	  						elseif ($tier == '5'){
-		  						$price = get_field('hvac_product_price_5');}
-		$output .= '<label>';
-				$output .= '<div class="div-box-product">';
-					//$output .= get_the_post_thumbnail($post_id, 'medium');        
-             $output .= get_the_post_thumbnail( null, 'medium');
-					$output .= '<h2>' . $title . '</h2>';
-// Check if $price is an empty string
-                  if (isset($price) && !empty($price)) {
-// Display the price
-                    $output .= '<div class="price" style="font-weight:bold;">$' . $price . '</div>';
-                  } else {
-// Display a default value or message
-                    $output .= '<div class="price" style="font-weight:bold;">Price not available</div>';
-                  }
-//$output .= '<div class="price" style="font-weight:bold;">$' . $price . '</div>'; 
-					$output .= '<p>' . $excerpt . '</p>';
-					$output .= '<input type="radio" name="product" value="' . $title . '" onclick="updateProductTitle(\'' . $title . '\', \'' . $price . '\')"></input>';
-				$output .= '</div>';
-		$output .= '</label>';
+    $price = 0;
+    if ($tier == '1'){
+      $price = get_field('hvac_product_price_1');}
+    elseif ($tier == '2'){
+      $price = get_field('hvac_product_price_2');}
+    elseif ($tier == '3'){
+      $price = get_field('hvac_product_price_3');}
+    elseif ($tier == '4'){
+      $price = get_field('hvac_product_price_4');}
+    elseif ($tier == '5'){
+      $price = get_field('hvac_product_price_5');}
+    elseif ($tier == '6'){
+       $price = get_field('hvac_product_price_6');}   
+    // Push the product data into the $products array
+    $products[] = [
+        'title' => $title,
+        'excerpt' => $excerpt,
+        'price' => $price,
+        'thumbnail' => get_the_post_thumbnail()
+    ];
   }
-  	$output .= '</div>';
-} else {
+  // Sort the products by price
+  usort($products, function($a, $b) {
+    return $a['price'] - $b['price'];
+  });
+  // Generate the output using the sorted $products array
+  foreach($products as $product) {
+      $output .= '<label>';
+      $output .= '<div class="div-box-product">';
+      $output .= $product['thumbnail'];        
+      $output .= '<h2>' . $product['title'] . '</h2>';
+      if (isset($product['price']) && !empty($product['price'])) {
+          $output .= '<div class="price" style="font-weight:bold;">$' . $product['price'] . '</div>';
+      } else {
+          $output .= '<div class="price" style="font-weight:bold;">Price not available</div>';
+      }
+      $output .= '<p>' . $product['excerpt'] . '</p>';
+      $output .= '<input type="radio" name="product" value="' . $product['title'] . '" onclick="updateProductTitle(\'' . $product['title'] . '\', \'' . $product['price'] . '\')"></input>';
+      $output .= '</div>';
+      $output .= '</label>';
+  }
+  $output .= '</div>';
+}
+ else {
 //Enter 404 Div
 	  $output .= '<div class="price" style="font-weight:bold;">';
 		  $output .= '<a href="/"><img src="https://bxbknowshvac.com/wp-content/uploads/BxB-Media-gray-horizontal-logo.svg" alt="No Product"></a>';
